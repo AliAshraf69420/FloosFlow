c17a35c8aa389ca451b53a6e87c206c6bed078bc
Ahmed Mohammady Mohammad <ahmedmohamady2005@gmail.com>
2025-11-29 21:28:17 +0200

    Resuble CSS color for components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,20 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ff-bg-light": "#FFFFFF",
+        "ff-bg-dark": "#1E1E1E",
+        "ff-input": "#2A2A2A",        
+        "ff-input-hover": "#333333",
+         "ff-accent": "#49EB8C",  
+      },
+      backgroundImage: {
+        "ff-gradient":
+          "linear-gradient(to right, #62A6BF, #49EB8C, #65E67F)",
+      },
+    },
   },
   plugins: [],
 };
