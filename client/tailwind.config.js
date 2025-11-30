c3c94b89f6ccdf8fc261b0ce3f3e32320d556701
Momen-Sanad <s-momen.sanad@zewailcity.edu.eg>
2025-11-30 09:49:33 +0200

    footer is now using defined classes and attributes from tailwind.config.js and index.css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#62A6BF',
+        secondary: '#49EB8C',
+        accent: '#65E67F',
+        footerBg: '#1E1E1E', // Dark footer background
+        footerText: '#B0B0B0', // Light gray footer text
+      },
+    },
   },
   plugins: [],
-};
+}
