2d5a7c7b21dc6c25e3b39343dc3d486cef70db3c
Abdullah Ayman <abdulluhayman@gmail.com>
2025-12-01 19:58:30 +0200

    Login and Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-ff-bg-dark text-gray-100 font-sans">
       <NavBar />
-
-      {
-        /* content here */
-      }
-
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ client/src/components/Login/LoginWelcomeCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginWelcomeCard = () => {
+    return (
+        <div className="ff-card p-6 sm:p-10 flex flex-col  sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-6 w-full max-w-[1100px] h-[200px] ">
+            <img src='../../public/logo.svg' alt='Logo' class='w-36 h-36 object-contain' />
+            <h1 class="text-3xl font-bold">Welcome Back!</h1>
+        </div>
+    )
+}
+
+export default LoginWelcomeCard
