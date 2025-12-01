4b3c09971a353af6e69498f7ec0cb399e08600c4
Ahmed Mohammady Mohammad <ahmedmohamady2005@gmail.com>
2025-12-01 09:58:29 +0200

    init step for Home Page

---
+++ client/src/components/Home/services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>Services</h2>
+            <Link to="/services">Services</Link>
+        </div>
+    );
+};
+
+export default Services;
