b147eae6de6f08e5e418224b2951cd549651816b
Abdullah Ayman <abdulluhayman@gmail.com>
2025-12-02 12:31:54 +0200

    Help page is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,21 @@ import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
-
+import { Routes, Route } from 'react-router-dom';
+import HelpPage from './routes/HelpPage';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-ff-bg-dark text-gray-100 font-sans">
       <NavBar />
-
-      {
-        /* content here */
-      }
-
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/Help" element={<HelpPage />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
