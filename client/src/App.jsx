afd28c03c7c38088b17f6a1a6ea62456316db418
Ali <AliAwesomethe22th@gmail.com>
2025-12-02 12:13:59 +0200

    finished transaction details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-import './App.css';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+// import Transactions from "./routes/Transactions";
+import TransactionDetailsPage from "./routes/TransactionDetails";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen bg-ff-bg-dark text-gray-100 font-sans">
       <NavBar />
-
-      {
-        /* content here */
-      }
-
+      <main className="flex-grow">
+        <Routes>
+          <Route
+            path="/TransactionDetails"
+            element={<TransactionDetailsPage />}
+          />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ client/src/routes/TransactionDetails.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TransactionDetailsCard from "../components/TransactionDetails/TransactionDetailsCard";
+
+export default function TransactionDetailsPage() {
+  return (
+    <main className="flex-grow pt-24 px-4 sm:px-6 lg:px-8 text-center">
+      <TransactionDetailsCard />
+    </main>
+  );
+}
