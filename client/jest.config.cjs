2ca3db39dc1d632702e379adaea0ffab432ed24f
AliAshraf69420 <AliAshraf69421@gmail.com>
2025-12-23 23:25:29 +0200

    fixed testing

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,8 +1,20 @@
 module.exports = {
   testEnvironment: "jest-environment-jsdom",
+
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
   },
+
   setupFilesAfterEnv: ["<rootDir>/src/test/setup.js"],
+
   moduleFileExtensions: ["js", "jsx"],
+
+  testPathIgnorePatterns: [
+    "/e2e/",
+    "e2e.spec.js",
+  ],
+
+  moduleNameMapper: {
+    "\\.(svg|png|jpg|jpeg|gif|webp)(\\?url)?$": "<rootDir>/__mocks__/fileMock.js",
+  },
 };
