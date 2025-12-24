876caa4f74fccb3596789bad1f647a2e7ee0c7a3
AliAshraf69420 <AliAshraf69421@gmail.com>
2025-12-24 03:22:11 +0200

    hmm

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173
-  },
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT ? Number(process.env.PORT) : 8080,
-    strictPort: false
+    allowedHosts: true
   }
 });
